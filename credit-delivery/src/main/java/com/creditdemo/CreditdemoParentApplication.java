6402bd351de22878de3ea114a85dd635e87f7c6c
chenhongding <21378@etransfar.com>
2020-05-29 14:11:43 +0800

    init

---
+++ credit-delivery/src/main/java/com/creditdemo/CreditdemoParentApplication.java
@@ -0,0 +1,13 @@
+package com.creditdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditdemoParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditdemoParentApplication.class, args);
+	}
+
+}
